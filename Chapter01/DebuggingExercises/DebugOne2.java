8b9d4ae38293d2805fd2e7f6f4ee54339ff0cecc
Tristan Du <tristandubohao@outlook.com>
2019-10-05 14:43:24 -0700

    i fix all most of the mistkes

--- Chapter01/DebuggingExercises/DebugOne2.java
+++ Chapter01/DebuggingExercises/DebugOne2.java
@@ -7,6 +7,6 @@ public class DebugOne2
       System.out.println("Getting a program to work");
       System.out.println("can be a challenge,");
       System.out.println("but when everything works correctly,");
-      System.out.println("tt's very satisfying");
+      System.out.println("it's very satisfying");
    }
 }
